14be09b76fba7d11462151475a9d1c4cbe5c369a
Duncan Temple Lang <duncan@r-project.org>
2013-05-13 09:54:40 -0700

    .

--- explorations/setEg.R
+++ explorations/setEg.R
@@ -1,20 +1,22 @@
 library(RCUDA)
 #cuInit()
-#.Call("R_cuDeviceGet", 0L)
-#.Call("R_cudaSetDevice", 0L)
-#ctx = createContext(0L)
 m = loadModule("inst/sampleKernels/fixedNumSet.ptx")
 #m = loadModule("inst/sampleKernels/set.ptx")
 k = m[["setValue_kernel"]]
+k = m$setValue_kernel
 
 N = 1e6L
 x = integer(N)
 
 mem = cudaMalloc(N)
+copyToDevice(integer(N), mem)
+# or simply
+mem = copyToDevice(x)
 
-#ans = .cuda(k, mem, inplace = TRUE, gridDim = c(1L, 1L, 1L), blockDim = c(32L, 1L, 1L))
+#copyToDevice(numeric(N), mem)
+#ans = .cuda(k, mem, inplace = TRUE, gridDim = c(1L, 1L, 1L), blockDim = c(1L, 1L, 1L))
 
-i = .Call("R_getCudaIntVector", mem, N)
+i = copyFromDevice(mem, N, "integer")
 
 #ans = .cuda(k, x, N, inplace = TRUE, gridDim = c(1L, 1L, 1L), blockDim = c(32L, 1L, 1L))
 
