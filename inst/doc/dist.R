17562f757027278579bcb62d00e87dd4f2c44f1f
Duncan Temple Lang <duncan@r-project.org>
2013-07-01 11:52:14 -0700

    working on the distance example

--- inst/doc/dist.R
+++ inst/doc/dist.R
@@ -1,16 +1,44 @@
 library(RCUDA)
-
 mod = loadModule("distance_gputools.ptx")
 
 
 N = c(A = 8000L, B = 3000L)
 p = 70L
 
-N = c(A = 20L, B = 11L)
+N = c(A = 5L, B = 7L)
 p = 9L
 
 A = matrix(rnorm(N["A"]*p), N["A"], p)
 B = matrix(rnorm(N["B"]*p), N["B"], p)
+
+AB = t(rbind(A, B))
+kernel = mod$euclidean_kernel_same
+ABref = copyToDevice(AB, elType = "float")
+ans = cudaMalloc(sum(N)^2, elType = "float")
+.gpu(kernel,
+     ABref, p, sum(N),
+     ABref, p, sum(N),
+     p, ans, 2.0,
+     outputs = FALSE, gridDim = N, blockDim = 32L)
+out = copyFromDevice(ans, sum(N)^2, "float")
+d.AB = matrix(out, sum(N), sum(N))
+
+
+if(FALSE) {
+  # Desired way to do this.
+AB = t(rbind(A, B))
+kernel = mod$euclidean_kernel_same
+ABref = copyToDevice(AB, elType = "float")
+out = .gpu(kernel,
+              ABref, p, sum(N),
+              ABref, p, sum(N),
+              p, ans = numeric(sum(N)^2), 2.0,
+              outputs = "ans", gridDim = N, blockDim = 32L)
+d.AB = matrix(out, sum(N), sum(N))
+}
+
+
+if(FALSE) {
 
 
   # 10 arguments
@@ -20,12 +48,6 @@ out = .gpu(k, t(A),  N["A"], p, t(B), N["B"], p, p, ans = numeric(prod(N)), N["A
                outputs = "ans", gridDim = N, blockDim = 32L)
 
 matrix(out, N["A"], N["B"])
+}
 
 
-AB = t(rbind(A, B))
-k = mod$euclidean_kernel_same
-out = .gpu(k, AB, sum(N), p,
-              AB, sum(N), p,
-              p, ans = numeric(sum(N) * sum(N)), sum(N), 2.0,
-              outputs = "ans", gridDim = N, blockDim = 32L)
-d.AB = matrix(out, N["A"], N["B"])
