e98827be365dcd142b6d0fffc3aed9cabfd63023
Duncan Temple Lang <duncan@r-project.org>
2016-07-17 18:53:49 -0700

    version information from CUDA and comparing install and runtime to give warning/error

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,7 +1,12 @@
 .onLoad =
 function(...)
 {
-  .Call("R_cuInit", 0L, FALSE)
+  if(!getOption("NoInitializeCUDAOnLoad", FALSE)) {
+    .Call("R_cuInit", 0L, FALSE)
+  }
+
+  compareCUDAVersions()
+  
   # temporary. Investigating a crash 
   # when unloading the DLL when using CUDA 5.5
   # This seems to avoid it.
@@ -9,3 +14,21 @@ function(...)
 }
 
 
+compareCUDAVersions =
+function(cur = cudaVersion(), orig = CudaVersionNumber)
+{
+  a = splitCUDAVersion( cur["runtime"] )
+  b = splitCUDAVersion( orig)
+
+  if(a["major"] != b["major"])
+      stop("RCUDA was installed for CUDA version ", b, " but run time is version ", a)
+  if(a["minor"] < b["minor"])
+      warning("RCUDA was installed for CUDA minor version ", b, " but run time is earlier than this: version ", a)  
+}
+
+
+splitCUDAVersion =
+function(val)
+{
+   c(major = as.integer(val/1000L), minor = as.integer(val%%100)/10L)
+}
